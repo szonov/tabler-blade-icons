2f4ebe78b7b5289834ad71c93c8a76d0d60a0a80
Sergey Zonov <sergey@zonov.com>
2023-03-11 16:53:49 +0700

    update iconset to 2.9.0, support laravel 10

---
+++ resources/views/components/icon/brand-fiverr.blade.php
@@ -0,0 +1,8 @@
+@props(['stroke' => 'currentColor','fill' => 'none'])
+
+<svg xmlns="http://www.w3.org/2000/svg" {{ $attributes->merge(['class' => 'icon icon-tabler icon-tabler-brand-fiverr']) }} width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke={{ $stroke }} fill={{ $fill }} stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M15 3h-2a6 6 0 0 0 -6 6h-3v4h3v8h4v-7h4v7h4v-11h-8v-1.033a1.967 1.967 0 0 1 2 -1.967h2v-4z" />
+</svg>
+
+
